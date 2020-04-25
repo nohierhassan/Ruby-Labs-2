22fbfc1de4d14ace2442a1be00443d17e0103ee5
Nohier <nohierassan2@gmail.com>
2020-04-25 17:04:11 +0200

    Create and Retrive are added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,29 @@
 class ArticlesController < ApplicationController
   def new
   end
+################################################################
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save()
+    redirect_to @article
   end
+  # we've extracted the allowance of the mass assignment of title and text just to call them in update method as well
+ ##################################################################################
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ #####################################################################################
+ private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 
 end
