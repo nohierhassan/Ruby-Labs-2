5dc92258bd9667e872639b0e5771e6de0d093284
Nohier <nohierassan2@gmail.com>
2020-04-26 15:30:06 +0200

    Validation added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,26 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 ################################################################
   def create
     @article = Article.new(article_params)
-    @article.save()
+    if @article.save()
     redirect_to @article
+    else 
+      render 'new'
+    end
   end
   # we've extracted the allowance of the mass assignment of title and text just to call them in update method as well
  ##################################################################################
   def show
     @article = Article.find(params[:id])
   end
+  #################################################################################
+  def index
+    @articles = Article.all
+  end
+
  
  
  
