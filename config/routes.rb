471d3785b7fecc43144263aecf006f9f38be4eeb
Nohier <nohierassan2@gmail.com>
2020-04-27 16:12:01 +0200

    Adding Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   resources :articles do
     resources :comments
   end
-  root 'welcome#index'
+  root to: "articles#index"
 end
